18f29cd4f400cda89963686d5ae0a9fe2cd96558
Gerry-Geraldy <whateveritakes10@gmail.com>
2022-06-23 16:27:47 +0800

    On Progress CRID PHP

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php
+$host   = 'localhost';
+$user   = 'root';
+$pass   = '';
+$db     = 'db_akademik';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //jika tidak terkoneksi
+    die('Tidak bisa terkoneksi ke database');
+}
+$nim        = '';
+$nama       = '';
+$alamat     = '';
+$fakultas   = '';
+$sukses     = '';
+$error      = '';
+
+if (isset($_POST['simpan'])) {
+    $nim        = $_POST['nim'];
+    $nama       = $_POST['nama'];
+    $alamat     = $_POST['alamat'];
+    $fakultas   = $_POST['fakultas'];
+
+    if ($nim && $nama && $alamat && $fakultas) {
+        $sql1   = "insert into mahasiswa(nim, nama, alamat, fakultas) values ('$nim', '$nama', '$alamat', '$fakultas')";
+        $q1     = mysqli_query($koneksi, $sql1);
+        if ($q1) {
+            $sukses     = "Berhasil memasukkan data mahasiswa";
+        } else {
+            $error      = "Gagal memasukkan data mahasiswa";
+        }
+    }else{
+        $error = "Silahkan masukkan semua data";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px;
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="mx-auto">
+        <!-- Untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Create / Edit Data
+            </div>
+            <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                }
+                ?>
+
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+                <?php
+                }
+                ?>
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nim" name="nim" value="<?php echo $nim ?>">
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $nama ?>">
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="alamat" name="alamat" value="<?php echo $alamat ?>">
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="fakultas" class="col-sm-2 col-form-label">Fakultas</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" name="fakultas" id="fakultas">
+                                <option value="">- Pilih Fakultas -</option>
+                                <option value="saintek" <?php if ($fakultas == "saintek") echo "selected" ?>>Saintek</option>
+                                <option value="soshum" <?php if ($fakultas == "soshum") echo "selected" ?>>Soshum</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- untuk mengeluarkan data-->
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Mahasiswa
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
